e0e049627b98304f6e1f7e94d0a4ab8a8518fbd8
telamonian <telamonian@users.noreply.github.com>
2021-09-07 08:38:14 -0400

    add `localhost` to yarn `unsafeHttpWhitelist` when local registry is started

--- buildutils/src/local-repository.ts
+++ buildutils/src/local-repository.ts
@@ -146,6 +146,13 @@ packages:
       'npmRegistryServer',
       local_registry
     ]);
+    child_process.execFileSync('yarn', [
+      'config',
+      'set',
+      'unsafeHttpWhitelist',
+      '--json',
+      '["localhost"]'
+    ]);
   } catch (e) {
     console.log('WARNING: unable to set registry in yarn config');
   }
@@ -240,9 +247,11 @@ async function stopLocalRegistry(out_dir: string) {
     child_process.execSync(
       `yarn config set npmRegistryServer ${data.prev_yarn}`
     );
+    child_process.execSync(`yarn config unset unsafeHttpWhitelist`);
   } else {
     try {
       child_process.execSync(`yarn config unset npmRegistryServer`);
+      child_process.execSync(`yarn config unset unsafeHttpWhitelist`);
     } catch (e) {
       console.log(
         'WARNING: unable to revert registry to previous value in yarn config'
