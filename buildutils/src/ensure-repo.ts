0b3c3f162db9496e58afd3ce118597f0bd19f086
Frederic Collonval <fcollonval@gmail.com>
2023-03-10 09:55:34 +0100

    Don't use $npm_execpath

--- buildutils/src/ensure-repo.ts
+++ buildutils/src/ensure-repo.ts
@@ -419,7 +419,7 @@ function ensureMetaPackage(): string[] {
   });
 
   // Add to build:all target
-  mpData.scripts['build:all'] = '$npm_execpath run build';
+  mpData.scripts['build:all'] = 'npm run build';
 
   // Write the files.
   if (messages.length > 0) {
