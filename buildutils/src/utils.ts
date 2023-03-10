af930ea78941d2dad068032be06071a9ab31d3a8
Nicolas Brichet <nicolas.brichet@quantstack.net>
2023-03-10 18:20:16 +0100

    TO BE REMOVE: print diff before bump

--- buildutils/src/utils.ts
+++ buildutils/src/utils.ts
@@ -242,6 +242,7 @@ export function prebump(): void {
     encoding: 'utf8'
   });
   if (status.length > 0) {
+    run('git diff');
     throw new Error(
       `Must be in a clean git state with no untracked files.
 Run "git status" to see the issues.
